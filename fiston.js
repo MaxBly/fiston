89e58c709e6c36832cc9f8c7a559b1032489ec83
MaxBly <max_bellamy@live.fr>
2019-01-31 13:01:06 +0000

    0

--- fiston.js
+++ fiston.js
@@ -14,7 +14,7 @@ var sio = require("socket.io")(server, {
     handlePreflightRequest: (req, res) => {
         const headers = {
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Origin": "http://bly-net.com:7000/", //or the specific origin you want to give access to,
             "Access-Control-Allow-Credentials": true
         };
         res.writeHead(200, headers);
