86bc2c30d1693884b17e59f8fff57a2f4463ede9
MaxBly <max_bellamy@live.fr>
2019-02-06 00:18:31 +0000

    fix socket words

--- fiston.js
+++ fiston.js
@@ -23,7 +23,7 @@ var discordbot = new FistonDjs(process.env.TOKEN);
 
 //init server
 app.use(cors());
-app.use('/app', twitbot.router);
+app.use('/fiston', twitbot.router);
 
 
 twitbot.schedule(time);
